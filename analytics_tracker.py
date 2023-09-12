44a5f607d9039f113a9e3b5ae0dadfcf3fc41b44
masinsight <126551615+mas-360@users.noreply.github.com>
2023-09-12 20:25:21 +0200

    Update analytics_tracker.py

--- analytics_tracker.py
+++ analytics_tracker.py
@@ -121,7 +121,7 @@ def _track_user():
         st.session_state.user_tracked = True
         counts["total_pageviews"] += 1
         counts["per_day"]["pageviews"][-1] += 1
-        st.write("Tracked new user":)
+        st.write("Tracked new user")
         st.write(counts["total_pageviews"])
         
 
